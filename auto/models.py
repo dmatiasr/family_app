66f3eb9421c26e16d7de067fdfce7d6d4cfa5b3f
mrondeau <matias.rondeau@onapsis.com>
2022-09-21 16:33:05 -0300

    Initial commit

---
+++ auto/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Automovil(models.Model):
+    name = models.CharField(max_length=50)
+    matricula = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    applied_date = models.DateField()
+    name = models.CharField(null=True, max_length=50)
+    description = models.TextField()
+    technician = models.CharField(null=True, max_length=50)
+    price = models.CharField(max_length=50)
+    related_vehicle = models.ForeignKey(Automovil, verbose_name="related_vehicle", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
