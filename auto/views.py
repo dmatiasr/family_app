2a74358ba5bcb494a6cdd04d177423c95e7982fd
mrondeau <matias.rondeau@onapsis.com>
2022-09-25 17:39:59 -0300

    - Add ./idea to .gitignore. -Modify serializer to ModelSerializer. -Add list fn to view. -UT's to support it

--- auto/views.py
+++ auto/views.py
@@ -2,6 +2,7 @@ from rest_framework import (
     permissions,
     viewsets,
 )
+from rest_framework.response import Response
 
 from .models import Automovil
 from .serializers import VehicleSerializer
@@ -11,3 +12,15 @@ class VehicleViewSet(viewsets.ModelViewSet):
     queryset = Automovil.objects.all()
     serializer_class = VehicleSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
