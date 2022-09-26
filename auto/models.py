f96bec6f0f2a68125c1782199f1b5073ad7ed561
matias.rondeau@onapsis.com <matias.rondeau@onapsis.com>
2022-09-26 08:53:16 -0300

    add UT for POST method support code

--- auto/models.py
+++ auto/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Automovil(models.Model):
     name = models.CharField(max_length=50)
-    matricula = models.CharField(max_length=50)
+    matricula = models.CharField(unique=True, max_length=50)
 
     def __str__(self):
         return self.name
